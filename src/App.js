151503c54fc810fb5e61cbff4ab513db272d06db
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-06-27 12:25:22 +0530

    add fetch and componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      name : 'bhanu'
-    }
+      monsters: [],
+    };
+  }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState(() => {
+      return {monsters : users}
+    },() =>{
+      console.log(this.state)
+    }))
   }
   render() {
-  return (
-    <div className="App">
-      <p>hello {this.state.name}</p>
-      <button onClick={() => {this.setState({name:'prakash'})}}>change name</button>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>
+                {" "}
+                hi my name is {monster.name}and my profession is {monster.company.bs}{" "}
+              </h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default App;

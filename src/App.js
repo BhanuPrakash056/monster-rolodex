e1f9e750a0bdd0b4b2e2e0f4ad290814608e4907
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-06-27 15:07:03 +0530

    added search option

--- src/App.js
+++ src/App.js
@@ -8,23 +8,38 @@ class App extends Component {
       monsters: [],
     };
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState(() => {
-      return {monsters : users}
-    },() =>{
-      console.log(this.state)
-    }))
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder="search" onChange={(event) => {
+          const search = event.target.value.toLocaleLowerCase();
+          const filterMonster = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(search);
+          });
+          this.setState(()=>{
+            return {monsters : filterMonster}
+          })
+        }} />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>
-                hi my name is {monster.name}and my profession is {monster.company.bs}{" "}
+                hi my name is {monster.name}and my profession is{" "}
+                {monster.company.bs}{" "}
               </h1>
             </div>
           );
